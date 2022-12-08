d93e5803ab9c225f676b67cb51b7feecdd0ee10c
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-12-08 16:35:12 -0300

    test: ensure calls CreateNewEventImpl and returns if success the created event

---
+++ src/test/java/com/api/eventmanager/domain/usecases/CreateNewEventTest.java
@@ -0,0 +1,50 @@
+package com.api.eventmanager.domain.usecases;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.api.eventmanager.domain.contracts.repositories.EventRepository;
+import com.api.eventmanager.domain.contracts.usecases.CreateNewEvent;
+import com.api.eventmanager.domain.entities.Event;
+import com.api.eventmanager.domain.errors.InvalidDataException;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class CreateNewEventTest {
+
+  @Autowired
+  CreateNewEvent sut;
+
+  Event event;
+
+  @Mock
+  EventRepository eventRepository;
+
+  @BeforeAll
+  public void mocks() {
+    this.event = new Event(1, "Any event", 1, new Date(), new Date());
+    EventRepository eventRepository = mock(EventRepository.class);
+    when(eventRepository.save(event)).thenReturn(event);
+    this.sut = new CreateNewEventImpl(eventRepository);
+  }
+
+  @Test
+  public void shouldBeCreateAndReturnValidEvent() {
+    try {
+      var result = sut.perform(this.event);
+      Assertions.assertEquals(result, event);
+      Assertions.assertTrue(event.isValid());
+    } catch (InvalidDataException e) {
+      e.printStackTrace();
+    }
+  }
+}
