a3306f628ceb672bc662957aac71e9747e3d385a
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-12-08 13:32:27 -0300

    test: ensure calls Event and returns valid entity

---
+++ src/test/java/com/api/eventmanager/domain/entities/EventTest.java
@@ -0,0 +1,32 @@
+package com.api.eventmanager.domain.entities;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class EventTest {
+  Event sut;
+
+  @BeforeEach
+  public void setup() {
+    this.sut = new Event();
+
+    this.sut.setId(1);
+    this.sut.setName("Next Level Week");
+    this.sut.setVacancies(2);
+    this.sut.setStartDate(new Date());
+    this.sut.setEndDate(new Date());
+  }
+
+  @Test
+  public void shouldBeReturnValidId() {
+    Assertions.assertEquals(1, this.sut.getId());
+  }
+
+  @Test
+  public void shouldBeValidateEntity() {
+    Assertions.assertEquals(true, this.sut.isValid());
+  }
+}
