81b4f09b15d29d98513158cdaa4050bca6fe696d
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-12-09 18:55:15 -0300

    refactor: remove declaration of SpringEventRepository and SpringUserRepository

--- src/main/java/com/api/eventmanager/infra/database/repositories/SpringEventRepository.java
+++ src/main/java/com/api/eventmanager/infra/database/repositories/SpringEventRepository.java
@@ -6,7 +6,5 @@ import org.springframework.stereotype.Repository;
 import com.api.eventmanager.infra.database.entities.EventEntity;
 
 @Repository
-@SuppressWarnings("unchecked")
 public interface SpringEventRepository extends JpaRepository<EventEntity, Long> {
-  EventEntity save(EventEntity event);
 }
--- src/main/java/com/api/eventmanager/infra/database/repositories/SpringUserRepository.java
+++ src/main/java/com/api/eventmanager/infra/database/repositories/SpringUserRepository.java
@@ -6,7 +6,5 @@ import org.springframework.stereotype.Repository;
 import com.api.eventmanager.infra.database.entities.UserEntity;
 
 @Repository
-@SuppressWarnings("unchecked")
 public interface SpringUserRepository extends JpaRepository<UserEntity, Long> {
-  UserEntity save(UserEntity event);
 }
