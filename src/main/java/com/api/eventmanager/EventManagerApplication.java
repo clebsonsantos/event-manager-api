0c09610df3b1f16e2b54b65f607fdc5e310a25a5
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-12-09 14:53:03 -0300

    refactor: remove declaration of SpringEventRepository

--- src/main/java/com/api/eventmanager/EventManagerApplication.java
+++ src/main/java/com/api/eventmanager/EventManagerApplication.java
@@ -4,14 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.api.eventmanager.infra.database.repositories.SpringEventRepository;
-
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = SpringEventRepository.class)
+@EnableJpaRepositories()
 public class EventManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventManagerApplication.class, args);
 	}
-
 }
